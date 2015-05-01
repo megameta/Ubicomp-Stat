5422b2c4e76fcdbccb9f5a5fa8d3988515bb09e5
Brian Creeden <brian.creeden@gmail.com>
2015-05-01 10:06:54 -0400

    New updates

--- Data Analysis.py
+++ Data Analysis.py
@@ -14,63 +14,68 @@ def aggregateTrials(tables):
     experiments = {}
     
     # Apparently its better to create the list of trials and then concat them all in one go, hence the two for loops.
+    # for frame in tables:
+        # if str(tables[frame].iat[0, 0]) not in trialsList:
+            # trialsList[str(tables[frame].iat[0, 0])] = [tables[frame][3:]]
+        # else:
+            # trialsList[str(tables[frame].iat[0, 0])].append(tables[frame][3:])
+    
+    listener = []
+    advertiser = []
+    
     for frame in tables:
-        if str(tables[frame].iat[0, 0]) not in trialsList:
-            trialsList[str(tables[frame].iat[0, 0])] = [tables[frame]]
-        else:
-            trialsList[str(tables[frame].iat[0, 0])].append(tables[frame])
-            
+        listener.append(tables[frame][:3])
+        advertiser.append(tables[frame][3:])
+        
+    
+    trialsList['Listener'] = listener
+    trialsList['Advertiser'] = advertiser
+        
+
+    print trialsList
     for experiment in trialsList:
-        experiments['Experiment ' + str(experiment)] = pd.concat(trialsList[experiment], ignore_index=True)
+        experiments[experiment] = pd.concat(trialsList[experiment], ignore_index=True)
     
     # experiments['Experiment 1'].to_csv('Raw_Data.csv', index=False)
     # experiments['Experiment 2'].to_csv('Raw_Data2.csv', index=False)
+    experiments['Listener'].to_csv('Raw_Data_Listener.csv', index=False)
+    experiments['Advertiser'].to_csv('Raw_Data_Advertiser.csv', index=False)
     
     return experiments
 
-def importData(file):
+# Use this to work with the raw data after it has been imported. Comment out the method that pulls in a new file so that 
+def importData(Experiment):
+    if Experiment < 3:
         dataFrames = {}
-        dataFrames['Experiment 2'] = pd.read_csv(file)
-        return dataFrames
-    
+        dataFrames['Experiment ' +str(Experiment)] = pd.read_csv('Raw_Data' +str(Experiment) + '.csv')
+    else:
+        dataFrames = {}
+        dataFrames['Listener'] = pd.read_csv('Raw_Data_Listener.csv', index=False)
+        dataFrames['Advertiser'] = pd.read_csv('Raw_Data_Advertiser.csv', index=False)
+        
+    return dataFrames
+        
 def cleanData(dataFrames):
     for frame in dataFrames:
         dataFrames[frame].rename(columns={' scan_record_converted':'pos'}, inplace=True)
         dataFrames[frame]['pos'] = dataFrames[frame]['pos'].apply(lambda x: int(x[4:]) * 1.2)
-        temp = dataFrames[frame]
-        # 
-        
-        # for index, row in temp.iterrows():
-
-        
-        #groups = temp.groupby('pos')
-        
-        #for name, group in groups:
-            #if name == 12:
-                #group[]
-        
-        # mask = temp.isin({'pos': [12]}).all(1)
-        # temp[mask, ' Remote_Bluetooth_Address':] = temp[mask, ' Scan_Mode':' bat_end_current_capacity']
-        
-        #temp1.to_csv('fixed rows.csv')
     
     return dataFrames
-
-def shiftPos10(row):
-    if row['pos'] == 12:
-        return row[:13] + row[12:-1]
-    else:
-        return row
-    #[' Remote_Bluetooth_Address':][' Scan_Mode':' bat_end_current_capacity']
     
 # Run the analysies(?) on the csv files. No cross file comparisons yet- just describing each of the experiments and trials.
 def dataAnalysis(dataFrames):
     percentCaptureResults = analyzePercentCapture(dataFrames['Experiment 2'])
     countResults = analyzeCount(dataFrames['Experiment 2'])
-    rssiResults = tempRSSI = analyzeRSSI(dataFrames['Experiment 2'])
+    rssiResults = analyzeRSSI(dataFrames['Experiment 2'])
     discoveryResults = analyzeInitialDiscovery(dataFrames['Experiment 2'])
     
-    captureStats = pd.concat([percentCaptureResults, countResults, rssiResults, discoveryResults], axis=1)
+    # print percentCaptureResults
+    print countResults
+    # print rssiResults
+    # print discoveryResults
+    
+    
+    captureStats = pd.concat([countResults, rssiResults, discoveryResults], axis=1)
     captureStats.columns = ['Percent_Captures', 'Capture_Count', 'RSSI', 'Time_to_Discovery']
     return captureStats
     
@@ -94,9 +99,11 @@ def plotStats(frame):
     # Actual plotting
     sns.set_style("ticks", {"xtick.minor.size":  12})
     sns.set_context('paper')
-
+    
+    frame.rename(columns={'Capture_Count': 'Discovery_Event_Count'}, inplace=True)
+    
     factors = ['Adv Latency','Listener Latency', 'Adv Power', 'pos']
-    variables = ['Time_to_Discovery', 'RSSI', 'Capture_Count', 'Percent_Captures']
+    variables = ['Time_to_Discovery', 'RSSI', 'Discovery_Event_Count', 'Percent_Captures']
 
     # for factor in factors:
         # for variable in variables:
@@ -104,11 +111,15 @@ def plotStats(frame):
             # plt.savefig( factor + ' vs. ' + variable + '.png', dpi=600, bbox_inches='tight')
             
     if True:
-        # sns.factorplot('Trial', y=variables[0], data=frame.dropna())
-        # plt.savefig('Trial' + ' vs. ' + variables[0] + '.png', dpi=600, bbox_inches='tight')
+        sns.factorplot('Trial', y=variables[0], data=frame.dropna())
+        plt.savefig('Trial' + ' vs. ' + variables[0] + '.png', dpi=600, bbox_inches='tight')
         for variable in variables:
             sns.factorplot(factors[2], variable, col=factors[0], row=factors[1], data=frame.dropna(), margin_titles=True, size=3, aspect=.8, x_order=['High','Medium','Low','Ultra_low'], col_order=['Low_Latency', 'Balanced', 'Low_Power'], row_order=['Low_Latency', 'Balanced', 'Low_Power'])
             plt.savefig('Trials' + ' vs. ' + variable + '.png', dpi=600, bbox_inches='tight')
+    
+    g = sns.factorplot(factors[3], variables[1], data=frame.dropna(), margin_titles=True, size=3, aspect=.8)
+    g.set_xticklabels(rotation=90)
+    plt.savefig(factors[3] + ' vs. ' + variables[1] + '.png', dpi=600, bbox_inches='tight')
     
         # for factor in factors:
             # sns.factorplot(factor, y=variables[0], data=frame.dropna())
@@ -122,7 +133,7 @@ def analyzePercentCapture (frame):
     
     trialByDevice = frame.groupby(' Trial_ID').apply(lambda x: x.groupby('pos').count()/x.count())
     
-    return trialByDevice[' percent_captures']    
+    return trialByDevice[' percent_captures']
 
 # Get the number of capture events by trial and position
 def analyzeCount(frame):
@@ -180,11 +191,11 @@ if __name__ == "__main__":
     pd.set_option('display.float_format', formatFloat)
     
     tables = {}
-    for filename in glob.glob('**\**\SCAN**'):
+    for filename in glob.glob('Battery Test\**\BATTERY**'):
         tables[filename] = pd.read_csv(filename)
         
     # experiments = aggregateTrials(tables)
-    experiments = importData('Raw_Data.csv')
+    experiments = importData(2)
     cleanedExperiments = cleanData(experiments)
     results = dataAnalysis(cleanedExperiments)
     factorizedResults = printToCSV(results)
